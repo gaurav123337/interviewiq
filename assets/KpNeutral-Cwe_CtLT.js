a1349889916a5de3c9cc0548782adc965e58dd0d
gaurav123337 <gaurav123337@users.noreply.github.com>
2026-08-23 10:12:18 +0000

    deploy: 2a04887e0f7957f0a53f30070f222cf5ef2eb905

--- assets/KpNeutral-Cwe_CtLT.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BSSbaIf2.js";function o({children:t}){return e.jsx("span",{className:"rounded-lg border border-line/10 bg-wht/10 px-2.5 py-1 text-[12.5px] font-semibold text-mut",children:t})}export{o as K};
