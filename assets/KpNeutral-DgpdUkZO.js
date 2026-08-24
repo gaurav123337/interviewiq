2fe9935cb545b0a300fa7541a5995c219d0d6a1c
gaurav123337 <gaurav123337@users.noreply.github.com>
2026-08-24 13:13:02 +0000

    deploy: 15bceb2184448258b695dd43c6fc750686a9c934

---
+++ assets/KpNeutral-DgpdUkZO.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DNgxrO1M.js";function o({children:t}){return e.jsx("span",{className:"rounded-lg border border-line/10 bg-wht/10 px-2.5 py-1 text-[12.5px] font-semibold text-mut",children:t})}export{o as K};
