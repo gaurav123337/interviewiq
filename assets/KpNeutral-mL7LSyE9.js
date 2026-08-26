65567cabc59a0b099dbfdfe7b7c5937de7fa4a42
gaurav123337 <gaurav123337@users.noreply.github.com>
2026-08-26 12:28:52 +0000

    deploy: abfb3b907da63d1db9a1ab6a360d68842a181d25

---
+++ assets/KpNeutral-mL7LSyE9.js
@@ -0,0 +1,1 @@
+import{v as e}from"./index-B1QTXK7J.js";function o({children:t}){return e.jsx("span",{className:"rounded-lg border border-line/10 bg-wht/10 px-2.5 py-1 text-[12.5px] font-semibold text-mut",children:t})}export{o as K};
